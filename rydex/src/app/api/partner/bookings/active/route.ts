8d2ea3d9ad7012b57951e6470475fe3f92f14cb6
DevaaPP <113279383+SelfCare69@users.noreply.github.com>
2026-06-03 20:26:54 +0530

    Feat: Proper Booking and Riders Collection v6.9

--- rydex/src/app/api/partner/bookings/active/route.ts
+++ rydex/src/app/api/partner/bookings/active/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
   const booking = await Booking.findOne({
     driver: session.user.id,
     status: {
-      $in: ["requested", "awaiting_payment", "confirmed", "started"],
+      $in: ["awaiting_payment", "confirmed", "started"],
     },
   }).sort({ createdAt: -1 }).populate("user driver vehicle")
   
